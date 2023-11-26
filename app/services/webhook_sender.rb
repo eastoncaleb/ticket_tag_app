ce9a0fb2bf4a873c428d2bbc8c9d3f8a3e11ca17
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-11-26 15:32:41 -0500

    Change the way tag webhook url is stored

--- app/services/webhook_sender.rb
+++ app/services/webhook_sender.rb
@@ -1,13 +1,12 @@
 class WebhookSender
   include HTTParty
+  TAG_URL = 'https://webhook.site/be8c40d5-653f-492b-a1b2-86efee0449bd'.freeze
 
   def self.send_tag_webhook(tag)
     return unless tag
 
-    webhook_url = 'https://webhook.site/be8c40d5-653f-492b-a1b2-86efee0449bd'
-
     HTTParty.post(
-      webhook_url,
+      TAG_URL,
       body: { tag: tag.name, count: tag.count }.to_json,
       headers: { 'Content-Type' => 'application/json' }
     )
