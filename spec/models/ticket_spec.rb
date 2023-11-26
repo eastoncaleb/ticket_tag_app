682be55ac00eb11ae1992c06e978338659d62add
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-11-26 16:28:22 -0500

    add validation check for more than 5 tags to ticket model spec

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -15,5 +15,18 @@ RSpec.describe Ticket, type: :model do
       ticket = Ticket.new(user_id: 123, title: nil)
       expect(ticket).not_to be_valid
     end
+
+    it 'is valid with five or fewer tags' do
+      ticket = Ticket.new(user_id: 123, title: 'Example Title')
+      5.times { ticket.tags.build(name: "tag#{rand(1000)}") }
+      expect(ticket).to be_valid
+    end
+
+    it 'is not valid with more than five tags' do
+      ticket = Ticket.new(user_id: 123, title: 'Example Title')
+      6.times { ticket.tags.build(name: "tag#{rand(1000)}") }
+      expect(ticket).not_to be_valid
+      expect(ticket.errors[:tags]).to include('can not have more than 5 tags')
+    end
   end
 end
