047752bc538c2eb25e333ff2102007ebbfbd1db5
Caleb Griffin <eastoncaleb.dev@gmail.com>
2023-12-02 17:38:29 -0500

    Refactor tickets controller and add additional specs

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,16 +1,22 @@
 class TicketsController < ApplicationController
   def create
     ticket = Ticket.new(ticket_params)
-    assign_tags(ticket) if tags.present?
+    response_message, status_code = nil, nil
 
-    if ticket.save
-      increment_tags_count(ticket) if tags.present?
-      send_tag_webhook
-      render json: { message: 'Ticket created successfully.' }, status: :created
-    else
-      errors = ticket.errors.full_messages.to_sentence || 'invalid parameters'
-      render json: { error: errors }, status: :unprocessable_entity
+    Ticket.transaction do
+      if tags_count_exceeded?
+        response_message, status_code = { error: 'can not have more than 5 tags' }, :unprocessable_entity
+      elsif ticket.save
+        handle_tags(ticket)
+        send_tag_webhook
+        response_message, status_code = { message: 'Ticket created successfully.' }, :created
+      else
+        errors = ticket.errors.full_messages.to_sentence || 'invalid parameters'
+        response_message, status_code = { error: ticket.errors.full_messages.to_sentence }, :unprocessable_entity
+      end
     end
+
+    render json: response_message, status: status_code
   end
 
   private
@@ -23,9 +29,17 @@ class TicketsController < ApplicationController
     @tags ||= params[:tags] ? params[:tags].map(&:downcase) : []
   end
 
+  def handle_tags(ticket)
+    return unless tags.present? && !tags_count_exceeded?
+
+    assign_tags(ticket)
+    increment_tags_count(ticket)
+  end
+
   def assign_tags(ticket)
+    existing_tags = Tag.where(name: tags).index_by(&:name)
     tags.each do |tag_name|
-      tag = Tag.find_or_create_by(name: tag_name)
+      tag = existing_tags[tag_name] || Tag.create(name: tag_name)
       ticket.tags << tag unless ticket.tags.include?(tag)
     end
   end
@@ -39,5 +53,9 @@ class TicketsController < ApplicationController
   def send_tag_webhook
     highest_count_tag = Tag.highest_count_tag
     WebhookSender.send_tag_webhook(highest_count_tag)
+  end
+
+  def tags_count_exceeded?
+    tags.count > 5
   end
 end
--- spec/requests/tickets_request_spec.rb
+++ spec/requests/tickets_request_spec.rb
@@ -37,5 +37,36 @@ RSpec.describe 'Tickets API', type: :request do
         expect(Ticket.count).to eq(0)
       end
     end
+
+    context 'when too many tags are provided' do
+      before { post '/tickets', params: { user_id: 1234, title: 'My title', tags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6'] } }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'does not create a ticket or tags' do
+        expect(Ticket.count).to eq(0)
+        expect(Tag.count).to eq(0)
+      end
+
+      it 'returns an error message about too many tags' do
+        expect(JSON.parse(response.body)['error']).to eq('can not have more than 5 tags')
+      end
+    end
+
+    context 'when there is a failure during ticket creation' do
+      before do
+        allow_any_instance_of(Ticket).to receive(:save).and_return(false)
+        post '/tickets', params: valid_attributes
+      end
+
+      it 'rolls back the entire transaction' do
+        expect(response).to have_http_status(422)
+        expect(Ticket.count).to eq(0)
+        # Assuming tags are only created if ticket saves successfully
+        expect(Tag.count).to eq(0)
+      end
+    end
   end
 end
